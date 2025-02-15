97bb35d3220377d5c3c12706ef2d24d267cd9a7d
YourName <your_email@example.com>
2025-02-15 12:10:35 -0500

    comments added to describe each line

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 /**
-* @param {number} f temperature in °F
-* @returns {number} temperature in °C
-*/
+ * @param {number} f temperature in °F (input)
+ * @returns {number} temperature in °C (output)
+ */
 function convertToCelcius(f) {
-    return (f-32) * (5 / 9);
+  return (f - 32) * (5 / 9); // conversion function f to c
 }
 
 /**
-* @param {number} f temperature in °F
-* @return {string} description 
-*/
+ * @param {number} f temperature in °F (input)
+ * @return {string} description (output)
+ */
 function describeTemperature(f) {
-    const c = convertToCelcius(f);
+  const c = convertToCelcius(f); // variable initialized with value of conversion function
 
-    let description = "";
+  let description = ""; // description variable contains string value
 
-    if (c < 0) {
-        description = "very cold";
-    } else if (c < 20) {
-        description = "cold";
-    } else if (c < 30) {
-        description = "warm";
-    } else if (c < 40) {
-        description = "hot";
-    } else (c < 50) {
-        description = "very hot";
-    }
+  if (c < 0) {
+    description = "very cold"; // if c < 0, description will be "very cold"
+  } else if (c < 20) {
+    description = "cold"; // if c < 20, description will be "cold"
+  } else if (c < 30) {
+    description = "warm"; // if c < 30, description will be "warm"
+  } else if (c < 40) {
+    description = "hot"; // if c < 40, description will be "hot"
+  } else {
+    description = "very hot"; // all temp > 40, description will be "very hot"
+  }
 
-    return `${f}°F is ${c}°C. That is ${description}.`;
+  return `${f}°F is ${c}°C. That is ${description}.`; // output: string `${f}F is ${c}C. That is ${description}.`
 }
 
-const f = prompt("Enter a temperature in farhenheit");
-const description = describeTemperature(f);
+const f = prompt("Enter a temperature in farhenheit"); // variable f contains prompt
+const description = describeTemperature(f); // description variable contains describeTemperature function
 alert(description);
